7cc2b4d6cd13d14837f81c41f91f1c2a65538d7c
raboudi <41745160+raboudi@users.noreply.github.com>
2019-05-19 15:47:30 +0200

    Update TCPserver.js

--- TCPserver.js
+++ TCPserver.js
@@ -2,7 +2,7 @@ var net = require('net');
 
 var HOST = '0.0.0.0';
 var PORT = 8498;
-var login_reply = [1,2,3,4,5,6];
+var login_reply = ["1","2"];
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
 // The sock object the callback function receives UNIQUE for each connection
