650298a435b556c47fc4add76b1ff98abd06bee4
raboudi <41745160+raboudi@users.noreply.github.com>
2019-05-20 11:10:35 +0200

    Update TCPserver.js

--- TCPserver.js
+++ TCPserver.js
@@ -2,7 +2,9 @@ var net = require('net');
 
 var HOST = '0.0.0.0';
 var PORT = 8498;
-var login_reply = new Buffer([0x24,0x4C]);
+//var login_reply = new Buffer([0x24,0x4C]);
+var login_reply = new Buffer('244C110D3879021E053ACAB','hex');
+'
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
 // The sock object the callback function receives UNIQUE for each connection
@@ -14,12 +16,11 @@ net.createServer(function(sock) {
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
         
-        console.log('DATA ' + sock.remoteAddress + ': ' + data.toString('hex'));
+        console.log('DATA received from ' + sock.remoteAddress + ': ' + data.toString('hex'));
         // Write the data back to the socket, the client will receive it as data from the server
         if (data.indexOf("$L") !== -1){
-        console.log("found $L ");
-        console.log('receive login command');
-        sock.write(login_reply);
+            console.log("found $L ");
+            sock.write(login_reply);
         }
         
     });
